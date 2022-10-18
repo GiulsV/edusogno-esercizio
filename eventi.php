0f74c42c1edd2bddd63566db27b2e6c0db370f59
GiulsV <valente.giulia93@gmail.com>
2022-10-18 13:05:22 +0200

    pagine php modificate + stile css (da finire lo stile e inserire le immagini nello sfondo + pagina eventi)

---
+++ eventi.php
@@ -0,0 +1,47 @@
+<?php include('server.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Edusogno</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+</head>
+
+<body>
+    <header>
+        <!-- logo -->
+    </header>
+    <section class="container">
+        <?php if (isset($_SESSION['success'])): ?>
+
+        <div>
+            <div class="containerEventi">
+                <h2>Ciao ecco i tuoi eventi</h2>
+                <div id="eventi">
+                    <?php if (count($eventiUtente) > 0): ?>
+                    <?php foreach ($eventiUtente as $evento): ?>
+
+                    <div class="evento">
+                        <div>
+                            <?= $evento['nome_evento'] ?>
+                        </div>
+                        <p>
+                            <?= $evento['data_evento'] ?>
+                        </p>
+                        <button name="join" class="button">JOIN</button>
+                    </div>
+
+                    <?php endforeach; ?>
+                    <?php else: ?>
+                    <h3>Non hai eventi</h3>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+
+        <?php endif ?>
+    </section>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Edusogno</title>
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+</head>
+
+<body>
+<header></header>
+
+	<section class="container">
+			<h1>Hai già un account?</h1>
+			<form method="post" action="login.php" id="formLogin">
+				<?php include('errors.php'); ?>
+					<label class="label">Inserisci l'e-mail</label>
+					<input class="input" type="text" name="email">
+					<span class="line" ></span>
+					<label class="label" >Inserisci la password</label>
+					<input class="input"  type="password" name="password">
+					<span class="line" ></span>
+					<button type="submit" class="button" name="login_user" id="buttonLogin">ACCEDI</button>
+				<p>
+					Non hai ancora un profilo? <a href="register.php">Registrati</a>
+				</p>
+			</form>
+	</section>
+</body>
+
+</html>
